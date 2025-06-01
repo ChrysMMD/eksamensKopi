3f158cf29a13e7d81bcfd738552278116a5bf973
ChrysMMD <chjo0001@stud.kea.dk>
2025-06-01 23:34:36 +0200

    clerk justering med redirect

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -13,7 +13,7 @@ export default function Nav() {
 
       <div className="flex justify-end items-center cursor-pointer p-4 gap-4 h-16">
         <SignedOut>
-          <SignInButton>
+          <SignInButton redirectUrl="/dashboard">
             <span className="text-white border border-white px-4 py-2 rounded hover:bg-gray-300/30">
               Log ind
             </span>
