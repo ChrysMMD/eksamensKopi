d5081fe5c678df6f02b3723cf2a1fb671970cd6e
ChrysMMD <chjo0001@stud.kea.dk>
2025-06-17 10:56:42 +0200

    Nav færdiggjort

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,21 +2,29 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Lock, Unlock } from 'lucide-react' 
 
 export default function Nav() {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
+  const iconTextColor = isHome ? "text-white" : "text-gray-500";
+  const logoSrc = isHome ? "/logo-white.png" : "/logo-black.png";
+
+
   return (
     <nav className="flex p-2 justify-between items-center z-10">
       <Link href="/">
-        <Image src="/logo.png" alt="Logo" width={70} height={40} priority />
+        <Image src={logoSrc} alt="Logo" width={70} height={40} priority />
       </Link>
 
       <div className="flex justify-end items-center cursor-pointer p-4 gap-4 h-16">
         <SignedOut>
           <SignInButton mode="redirect" redirectUrl="/dashboard">
-           <div className="flex items-center gap-1 text-white hover:opacity-80 cursor-pointer">
-              <Lock size={20} />
+            <div className={`flex items-center gap-1 hover:opacity-80 cursor-pointer ${iconTextColor}`}>
+              <Lock size={20} className={iconTextColor} />
               <span className="text-sm">Log ind</span>
             </div>
           </SignInButton>
@@ -24,8 +32,8 @@ export default function Nav() {
 
          <SignedIn>
           <UserButton signOutUrl="/" forceRedirectUrl="/" />
-         <Link href="/dashboard" className="flex items-center gap-1 text-white hover:opacity-80">
-            <Unlock size={20} />
+         <Link href="/dashboard" className={`flex items-center gap-1 hover:opacity-80 ${iconTextColor}`}>
+            <Unlock size={20} className={iconTextColor} />
             <span className="text-sm">Dashboard</span>
           </Link>
         </SignedIn>
